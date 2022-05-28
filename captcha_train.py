703f2cff07884ce38481f7de3824eaaebc12a67d
18sy36Songyu <71418971+shallowManica@users.noreply.github.com>
2022-05-28 15:27:33 +0800

    Add files via upload

---
+++ captcha_train.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 -*-
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import my_dataset
+from captcha_cnn_model import CNN
+
+gpu_count = torch.cuda.device_count()
+print("gpu_count=",gpu_count)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device1 = torch.device("cuda:1")
+print(torch.cuda.is_available())
+if_cuda= True
+gpu_count= 1
+
+# Hyper Parameters
+num_epochs = 15
+batch_size = 100
+learning_rate = 0.01
+
+def main():
+
+    cnn = CNN().to(device)
+    cnn.train()
+    print('init net')
+    criterion = nn.MultiLabelSoftMarginLoss()
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+    # Train the Model
+    train_dataloader = my_dataset.get_train_data_loader()
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_dataloader):
+            images = Variable(images).to(device)
+            labels = Variable(labels.float()).to(device)
+            predict_labels = cnn(images)
+            # print(predict_labels.type)
+            # print(labels.type)
+            loss = criterion(predict_labels, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 10 == 0:
+                print("epoch:", epoch, "step:", i, "loss:", loss.item())
+            if (i+1) % 100 == 0:
+                torch.save(cnn.state_dict(), "./model.pkl")   #current is model.pkl
+                print("save model")
+        print("epoch:", epoch, "step:", i, "loss:", loss.item())
+    torch.save(cnn.state_dict(), "./model.pkl")   #current is model.pkl
+    print("save last model")
+
+if __name__ == '__main__':
+    main()
+
+
